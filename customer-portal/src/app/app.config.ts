2c69ee6ee6e49bf620a06c1a1b4a5299eef8822d
mohamedaliznidi <mohamedali@fitnetmanager.com>
2024-09-10 14:58:20 +0100

    Update app.config.ts to add HTTP_INTERCEPTORS provider and CustomHttpInterceptor service

--- customer-portal/src/app/app.config.ts
+++ customer-portal/src/app/app.config.ts
@@ -1,13 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
-
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { CustomHttpInterceptor } from './shared/custom-http-interceptor.service';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
-    provideAnimations()
+    provideAnimations(),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: CustomHttpInterceptor,
+      multi: true
+    }
   ],
 };
---
+++ customer-portal/src/app/shared/custom-http-interceptor.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class CustomHttpInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const clonedRequest = req.clone({
+      setHeaders: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    });
+    return next.handle(clonedRequest);
+  }
+}

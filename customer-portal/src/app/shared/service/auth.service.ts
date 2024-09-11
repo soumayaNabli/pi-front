98a0d07a9577733cd3823a5d4ebfb511bbde579b
mohamedaliznidi <mohamedali@fitnetmanager.com>
2024-09-11 01:20:34 +0100

    Refactor AuthService login method to accept 'password' instead of 'mdp' and log response to console

--- customer-portal/src/app/shared/service/auth.service.ts
+++ customer-portal/src/app/shared/service/auth.service.ts
@@ -10,22 +10,26 @@ import { User } from './user.service';
 })
 export class AuthService {
   baseUrl = `${URL_PATH}/user`;
-    private userSubject = new BehaviorSubject<User[]>([]);
-    user$ = this.userSubject.asObservable();
+  private userSubject = new BehaviorSubject<User[]>([]);
+  user$ = this.userSubject.asObservable();
   private isAuthenticated = false;
   private userRole: string | null = null;
   private userName: string | null = null;
 
-  constructor(private router: Router,private http: HttpClient) {}
+  constructor(private router: Router, private http: HttpClient) { }
 
-  login(username:string,password:string): void {
+  login(username: string, password: string): void {
     console.log("🚀 ~ AuthService ~ login ~ mdp:", password)
     console.log("🚀 ~ AuthService ~ login ~ username:", username)
-    this.http.post(`${this.baseUrl}/login`,{username,password}).subscribe(res => console.log(res))
-    this.userName = username;
-    this.isAuthenticated = true;
-    this.userRole = "admin";
-    this.router.navigate(['/'+this.userRole]);
+    this.http.post(`${this.baseUrl}/login`, { username, password }).subscribe(res => {
+      console.log(res);
+      this.userName = username;
+      this.isAuthenticated = true;
+      this.userRole = "admin";
+    }
+    )
+
+    this.router.navigate(['/' + this.userRole]);
   }
 
   getUser(): any {
